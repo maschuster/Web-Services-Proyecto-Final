9dfe24518e41585c3fc7be5b3bcd086c1958607a
unknown <41472183@A-CRO-05.ALMAGRO.ort.arg>
2016-06-10 09:39:53 -0300

    BD

---
+++ mydb.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-06-2016 a las 14:36:41
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE IF NOT EXISTS `eventos` (
+  `idEvento` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `lugar` varchar(45) DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `foto` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idEvento`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+
+--
+-- Volcado de datos para la tabla `eventos`
+--
+
+INSERT INTO `eventos` (`idEvento`, `nombre`, `fecha`, `lugar`, `descripcion`, `foto`) VALUES
+(1, 'Fiestita', '2016-05-29 00:00:00', 'Yatay 240', 'ESTO SE VA A DESCONTROLAAAAR', 'Amigos.jpg'),
+(2, 'Asado', '2016-06-15 00:00:00', 'Medrano 232', 'La entrada al campo sale $45', 'Macabi.jpg'),
+(14, 'ASDASD', '2016-04-27 00:00:00', 'ASDASD', 'ASDASD', NULL),
+(15, 'ASDAS', '2016-04-27 00:00:00', 'DASDASD', 'ASDASD', NULL),
+(16, 'ASDAS', '2016-04-27 00:00:00', 'DASDASD', 'ASDASD', NULL),
+(17, 'ASDAS', '2016-04-27 00:00:00', 'DASDASD', 'ASDASD', NULL),
+(18, ')";;', '2016-04-27 00:00:00', 'DASDASD', 'ASDASD', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `objetos`
+--
+
+CREATE TABLE IF NOT EXISTS `objetos` (
+  `idObjeto` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `precio` int(11) DEFAULT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `idEvento` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`idObjeto`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `objetos`
+--
+
+INSERT INTO `objetos` (`idObjeto`, `nombre`, `precio`, `estado`, `idEvento`, `idUsuario`) VALUES
+(1, 'Coca-Cola', 25, 0, 0, 0),
+(2, 'Vacio', 70, 0, 0, 0),
+(3, 'Huevos', 20, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE IF NOT EXISTS `personas` (
+  `idPersonas` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Apellido` varchar(45) DEFAULT NULL,
+  `Fecha de Nacimiento` datetime DEFAULT NULL,
+  `Mail` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idPersonas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `mail` varchar(50) NOT NULL,
+  `fechadenacimiento` date NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
