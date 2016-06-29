83f6fbe79c630300f1253eb3702cdc5d0ba37b78
maschuster <m.a.schuster98@gmail.com>
2016-06-29 17:19:16 -0300

    Intentando arreglar agregar persona

--- agregarpersona.php
+++ agregarpersona.php
@@ -10,12 +10,12 @@ if (mysqli_connect_errno()) {
 
 $string = file_get_contents('php://input'); 
 $persona = json_decode($string, true);
-$query = "INSERT INTO personas (nombre, idEvento) values (?, ?)";
+$query = "INSERT INTO personas (idEvento, nombre) values (?, ?)";
 $stmt = $con->prepare($query);
 $stmt->bind_param(
 	'ss',
-	$persona["nombre"],
-	$persona["idEvento"]
+	$persona["idEvento"],
+	$persona["nombre"]
 );
 $stmt->execute();
 $res = $stmt->get_result();
