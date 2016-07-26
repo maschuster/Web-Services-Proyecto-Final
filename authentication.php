0b5ddaaaa8462aac62b1abd4f91f7d63dd5172a1
maschuster <m.a.schuster98@gmail.com>
2016-07-26 01:21:49 -0300

    Alabado dios del PHP, ayudame

--- authentication.php
+++ authentication.php
@@ -7,19 +7,18 @@ if (mysqli_connect_errno()) {
 	die("Failed to connect to MySQL" . mysqli_connect_error());
 }
 
-$string = file_get_contents('php://input'); 
-$token = json_decode($string, true);
+$jsonToken = file_get_contents('php://input'); 
+$token = json_decode($jsonToken, true);
 $url = "https://graph.facebook.com/me?access_token=". $token;
 $json = @file_get_contents($url);
 
 if($json != false){
     $e = json_decode($json, true);
-		
 	$name = $e["name"];
     $id = $e["id"];
-	$user = array('idFacebook'=> $id, 'name'=> $name);
+	$user = array('idFacebook'=> $id, 'nombre'=> $name);
 	
-	$query = 'SELECT * FROM usuarios WHERE idFacebook =' . $id;
+	$query = 'SELECT * FROM usuarios WHERE idFacebook =' . $user["idFacebook"];
 	$result = mysqli_query($con, $query);
 	if (mysql_num_rows($result)==0) 
 	{
@@ -28,7 +27,7 @@ if($json != false){
 		$stmt->bind_param(
 			'ss',
 			$user["idFacebook"],
-			$user["name"]
+			$user["nombre"]
 		);
 		$stmt->execute();
 		$res = $stmt->get_result();
