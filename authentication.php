38c8c5ffb2551105b038ee2a3c810b959ce80e6d
maschuster <m.a.schuster98@gmail.com>
2016-07-27 00:16:33 -0300

    APARENTEMENTE SOLUCIONADO

--- authentication.php
+++ authentication.php
@@ -9,7 +9,6 @@ if (mysqli_connect_errno()) {
 
 $jsonToken = file_get_contents('php://input'); 
 $token = json_decode($jsonToken, true);
-echo $token["accesToken"];
 $url = "https://graph.facebook.com/me?access_token=". $token["accesToken"];
 $json = @file_get_contents($url);
 
