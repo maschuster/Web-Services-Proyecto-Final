d5ddaea51c10e76a038cfb225552a948c97540b6
MATIAS ILAN KLOTZ <41472183@ort>
2016-09-02 09:41:08 -0300

    no se agregaba el usuario

--- authentication.php
+++ authentication.php
@@ -10,30 +10,28 @@ $json = @file_get_contents($url);
 
 if($json != false){
 	
-$e = json_decode($json, true);
-$name = $e["name"];
-$id = $e["id"];
-$user = array('idFacebook'=> $id, 'nombre'=> $name);
+	$e = json_decode($json, true);
+	$name = $e["name"];
+	$id = $e["id"];
+	$user = array('idFacebook'=> $id, 'nombre'=> $name);
 
-$query = 'SELECT * FROM usuarios WHERE idFacebook = ' . $user["idFacebook"];
-$result = mysqli_query($con, $query);
-if ($result == false) {
-	$query = "INSERT INTO usuarios (idFacebook,nombre) values (?, ?)";
-	$stmt = $con->prepare($query);
-	$stmt->bind_param(
-		'ss',
-		$user["idFacebook"],
-		$user["nombre"]
-	);
-	$stmt->execute();
-	$res = $stmt->get_result();
-}
-
-$res = json_encode($user,JSON_PRETTY_PRINT);
-echo $res;
-
-}else{
-	die("Invalido");
+	$query = 'SELECT * FROM usuarios WHERE idFacebook = ' . $user["idFacebook"];
+	$resultado = mysqli_insert_id($con);
+	if ($resultado > 1) {
+			die("Ya creado");
+	}else{
+		$query = "INSERT INTO usuarios (idFacebook,nombre) values (?, ?)";
+		$stmt = $con->prepare($query);
+		$stmt->bind_param(
+			'ss',
+			$user["idFacebook"],
+			$user["nombre"]
+		);
+		$stmt->execute();
+		$res = $stmt->get_result();
+	}
+	$res = json_encode($user,JSON_PRETTY_PRINT);
+	echo $res;
 }
 
 mysqli_close($con);
