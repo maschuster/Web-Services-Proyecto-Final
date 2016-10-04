de98db95cb858c603d2903348bc9205ca63d0cc2
maschuster <m.a.schuster98@gmail.com>
2016-10-04 19:48:03 -0300

    se actualiza la tabla preguntas cuando se vota, no solo las respuestas

--- votar.php
+++ votar.php
@@ -16,5 +16,18 @@ $stmt->bind_param(
 $stmt->execute();
 $res = $stmt->get_result();
 
+if($respuesta["voto"] == 1){
+	$query = "UPDATE preguntas SET afirmativos = afirmativos+1 WHERE idPregunta = ?";
+}else{
+	$query = "UPDATE preguntas SET negativos = negativos+1 WHERE idPregunta = ?";
+}
+
+$stmt = $con->prepare($query);
+$stmt->bind_param(
+	's',
+	$respuesta["idPregunta"]
+);
+$stmt->execute();
+$res = $stmt->get_result();
 mysqli_close($con);
 ?>
