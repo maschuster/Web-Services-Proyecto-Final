53f4c014e3a23be7ceeeb0316fe5393bb506324a
maschuster <m.a.schuster98@gmail.com>
2016-11-03 16:52:48 -0300

    arreglando fecha y hora cuando se agrega evento

--- agregarevento.php
+++ agregarevento.php
@@ -5,7 +5,7 @@ $con = getConnection();
 $user = getCurrentUser();
 $string = file_get_contents('php://input'); 
 $evento = json_decode($string, true);
-$query = "INSERT INTO eventos (idAdmin,nombre,fecha,lugar,descripcion,foto, hora) values (?, ?, ?, ?, ?, ?, ?)";
+$query = "INSERT INTO eventos (idAdmin,nombre,fecha,lugar,descripcion,foto) values (?, ?, ?, ?, ?, ?)";
 $stmt = $con->prepare($query);
 $stmt->bind_param(
 	'ssssss',
@@ -14,32 +14,39 @@ $stmt->bind_param(
 	$evento["fecha"],
 	$evento["lugar"],
 	$evento["descripcion"],
-	$evento["hora"],
 	$evento["foto"]	
 );
 
 $stmt->execute();
 $stmt->get_result();
 $res = mysqli_insert_id($con);
-if($res>1){
+if($res>1)
+{
 	$query = "INSERT INTO participantes (idFacebook,nombre,idEvento) values (?, ?, ?)";
-$stmt = $con->prepare($query);
-$stmt->bind_param(
-	'sss',
-	$user["idFacebook"],
-	$user["nombre"],
-	$res
-);
-$stmt->execute();
-$stmt->get_result();
-$resultado = mysqli_insert_id($con);
-if($resultado < 1){
-	http_response_code(500);
-	die("Error agregando Participante");
+	$stmt = $con->prepare($query);
+	$stmt->bind_param(
+		'sss',
+		$user["idFacebook"],
+		$user["nombre"],
+		$res
+	);
+	$stmt->execute();
+	$stmt->get_result();
+	$resultado = mysqli_insert_id($con);
+	
+	if($resultado < 1){
+		http_response_code(500);
+		die("Error agregando Participante");
+		echo = "Mensaje" =>"Error";
+	}else{
+        echo = "Mensaje" =>"Correcto";
+	}	
 }
-}else{
+else
+{
 	http_response_code(500);
 	die("Error agregando Evento");
+	echo = "Mensaje" =>"Error";
 }
 mysqli_close($con);
 ?>
