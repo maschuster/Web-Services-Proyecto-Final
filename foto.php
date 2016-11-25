8e926f491776402d499a4bdd97a4585040a00e30
maschuster <m.a.schuster98@gmail.com>
2016-11-24 23:05:12 -0300

    asd

--- foto.php
+++ foto.php
@@ -3,13 +3,12 @@ require_once("conexion.php");
 $con = getConnection();
 
 $id=$_GET["id"];
-$query = "SELECT foto FROM eventos WHERE id=$id";
+$query = "SELECT foto FROM eventos WHERE idEvento=$id";
 $query_exec = mysqli_query($con, $query);
 if(mysqli_num_rows($query_exec)){
 	$row=mysqli_fetch_assoc($query_exec);
 	$foto = $row["foto"];
 }
 header("Content-Type: image/jpeg");
-echo '<img src="data:image/jpeg;base64,'.base64_encode($foto).'"/>';
-//echo $foto;
+echo $foto;
 ?>
