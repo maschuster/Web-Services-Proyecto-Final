4754efff488e8f34e6a811f6354fda1f090d4a05
maschuster <m.a.schuster98@gmail.com>
2016-11-24 21:26:02 -0300

    creo que debería funcar

--- foto.php
+++ foto.php
@@ -1,9 +1,11 @@
 <?php
 require_once("conexion.php");
+$con = getConnection();
+
 $id=$_GET["id"];
 $tabla=$_GET["tabla"];
 $query = "SELECT foto FROM ".$tabla." WHERE id=".$_GET["id"];
-$query_exec = mysqli_query($GLOBALS["CONN"], $query);
+$query_exec = mysqli_query($con, $query);
 if(mysqli_num_rows($query_exec)){
 	$row=mysqli_fetch_assoc($query_exec);
 	$foto = $row["foto"];
