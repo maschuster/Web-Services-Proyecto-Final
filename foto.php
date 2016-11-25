e247ed6b12928d9fefd4b9c1abfd5ddfb9fc8c23
maschuster <m.a.schuster98@gmail.com>
2016-11-24 22:42:29 -0300

    q git commit -a -m asd

--- foto.php
+++ foto.php
@@ -3,8 +3,7 @@ require_once("conexion.php");
 $con = getConnection();
 
 $id=$_GET["id"];
-$tabla=$_GET["tabla"];
-$query = "SELECT foto FROM eventos WHERE id=".$_GET["id"];
+$query = "SELECT foto FROM eventos WHERE id=$id";
 $query_exec = mysqli_query($con, $query);
 if(mysqli_num_rows($query_exec)){
 	$row=mysqli_fetch_assoc($query_exec);
