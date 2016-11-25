04e4a138f09449155a40851e1a4401f5ee1a95b4
maschuster <m.a.schuster98@gmail.com>
2016-11-24 22:30:41 -0300

    asd

--- foto.php
+++ foto.php
@@ -4,12 +4,12 @@ $con = getConnection();
 
 $id=$_GET["id"];
 $tabla=$_GET["tabla"];
-$query = "SELECT foto FROM ".$tabla." WHERE id=".$_GET["id"];
+$query = "SELECT foto FROM eventos WHERE id=".$_GET["id"];
 $query_exec = mysqli_query($con, $query);
 if(mysqli_num_rows($query_exec)){
 	$row=mysqli_fetch_assoc($query_exec);
 	$foto = $row["foto"];
 }
-header("Content-Type: image/png");
+header("Content-Type: image/jpeg");
 echo $foto;
 ?>
